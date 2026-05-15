00d35b39a4bc81025b83d89c5f756bad3aa1795d
massimorampazzo-crypto <massimo.rampazzo@venicegeneralstore.it>
2026-05-15 14:43:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ email_dest = st.text_input(
 )
 
 GMAIL_USER = "selloutreportsvgs@gmail.com"
-GMAIL_PASSWORD = "wndl kafp xndj vbwu"
+GMAIL_PASSWORD = "wndlkafpxndjvbwu"
 
 
 def generate_report(df):
